2ac03d79345600e14963ea73eb966a0123be991d
Luis <leparrav+deel@gmail.com>
2022-07-01 13:07:02 +0300

    Create best professions API endpoint

--- src/routes/admins-routes.ts
+++ src/routes/admins-routes.ts
@@ -5,8 +5,11 @@ import { getProfile } from '../middleware/getProfile'
 const router = express.Router();
 const controller = new AdminController();
 
-router.route('/admin/best-profession?start=<date>&end=<date>').get(getProfile, (req, response) => controller.getBestProfession(req as any, response));
-router.route('/admin/best-clients?start=<date>&end=<date>&limit=<integer>').get(getProfile, (req, response) => controller.getBestClients(req as any, response));
+router.route('/admin/best-profession')
+.get((req, response) => controller.getBestProfession(req as any, response));
+
+router.route('/admin/best-clients')
+.get((req, response) => controller.getBestClients(req as any, response));
 
 
 export const adminRouter = router;
--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -3,12 +3,31 @@ import axios from 'axios';
 
 const serverUrl = 'http://localhost:3001';
 
-xdescribe('Admin integration test ', () => {
+describe('Admin integration test ', () => {
   describe('Admin REST API', () => {
-    it('GETs /contracts/:id a valid contract', async () => {
-      
-      const response = await axios.get(`${serverUrl}/contracts/1`, {});
-      expect(response.status).toBe(204);
+    it('POST /admin/best-profession for a valid date range', async () => {
+
+      const response = await axios.get(`${serverUrl}/admin/best-profession?start=2022-01-01&end=2022-08-01`);
+      expect(response.status).toBe(200);
+      expect(response.data[0].firstName).toBe('Alan');
+      expect(response.data[0].lastName).toBe('Turing');
+      expect(response.data[0].profession).toBe('Programmer');
+    });
+
+    it('POST /admin/best-profession for a invalid date range', async () => {
+      try {
+        await axios.get(`${serverUrl}/admin/best-profession?start=2022-11-01&end=2022-08-01`);
+      } catch (error) {
+        expect(error.response.status).toBe(400);
+      }
+    });
+
+    it('POST /admin/best-profession for not valid dates', async () => {
+      try {
+        await axios.get(`${serverUrl}/admin/best-profession?start=asdsa123&end=2023-132-d1`);
+      } catch (error) {
+        expect(error.response.status).toBe(400);
+      }
     });
   });
 });
